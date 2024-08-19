e632b53e9694b3c7369343ac41449c91310f11cd
Federico Irrazabal <fnirrazabal@gmail.com>
2024-08-18 21:08:17 -0300

    add next_public_url

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -11,7 +11,7 @@ export default function Login() {
     const { data } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`,
         queryParams: {
           access_type: "offline",
           prompt: "consent",
